3ea5c7fd32bd5e1abe57ae45dff7aef910217d4a
john.banks <johndbanksm@gmail.com>
2022-12-05 18:35:24 +0000

    fixed CSS + added logics/useState

--- src/js/component/TrafficLight.jsx
+++ src/js/component/TrafficLight.jsx
@@ -1,18 +1,60 @@
-import React from "react";
+import React, {useState} from "react";
 
 //include images into your bundle
 
 
 //create your first component
 const TrafficLight = () => {
-	return (
-		<><div id="trafficTop"></div>
-		<div id="container">
-			<div className="red light" onClick={() => this.setState({clickedLight: 'red'})}></div>
-			<div className="yellow light" onClick={() => this.setState({clickedLight: 'yellow'})}></div>
-			<div className="green light" onClick={() => this.setState({clickedLight: 'green'})}></div>
-		</div></>
-	);
+	const [lightRed, setLightRed] = useState("")
+	const [lightYellow, setLightYellow] = useState("")
+	const [lightGreen, setLightGreen] = useState("")
+	const [lightNone, setLightNone] = useState("")
+
+	const onActive = (color) => {
+		if (color == "red") {
+			setLightRed("activeL");
+			setLightYellow("");
+			setLightGreen("");
+		} else if (color == "yellow"){
+			setLightRed("");
+			setLightYellow("activeL");
+			setLightGreen("");
+		} else if (color == "green"){
+			setLightRed("");
+			setLightYellow("");
+			setLightGreen("activeL");
+		} // Reset button logic
+		else if (color == "none"){
+			setLightRed("");
+			setLightYellow("");
+			setLightGreen("");
+		}
+	};
+
+		return (
+			<>
+			{/* className = activeL (switch's between active light color) */}
+				<div className="traffic-container">
+					<div className="traffic-pole"></div>
+					<div className="light-container">
+						<div 
+							className={`light red ${lightRed}`} 
+							onClick={ () => onActive("red")} 
+							></div>
+						<div className={`light yellow ${lightYellow}`} 
+							onClick={ () => onActive("yellow")}
+							></div>
+						<div className={`light green ${lightGreen}`} 
+							onClick={ () => onActive("green")}
+							></div>
+					</div>
+					{/* reset button */}
+					<button className={`button none ${lightNone}`}
+						onClick={ () => onActive("none")}
+					>reset</button>
+				</div>
+			</>
+		);
 };
 
 export default TrafficLight;
---
+++ src/js/component/home.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+import TrafficLight from "./TrafficLight.jsx";
+
+//create your first component
+const Home = () => {
+    return(
+        <TrafficLight/>
+    );
+};
+
+export default Home;
